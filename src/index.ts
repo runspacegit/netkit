a6d014254be688772678b5aea648783e6f7ec9d3
Mikhail Marynenko <0x77dev@protonmail.com>
2020-06-27 22:44:37 +0300

    feat: 🎸 Ataraxia Network Integration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-export class NetKit {
+import { Network } from "ataraxia";
+import { NetKitConfiguration } from "./index.interfaces";
 
+export class NetKit {
+    public net: Network;
+    constructor({ authentication, endpoint, name }: NetKitConfiguration) {
+        this.net = new Network({
+            name: name ? name : "runspace",
+            authentication,
+            endpoint
+        });
+    }
+
+    public async start(): Promise<void> {
+        await this.startNetwork();
+    }
+
+    public async stop(): Promise<void> {
+        await this.stopNetwork();
+    }
+
+    private async startNetwork(): Promise<void> {
+        await this.net.start();
+    }
+
+    private async stopNetwork(): Promise<void> {
+        await this.net.stop();
+    }
 }
 
 export default NetKit;
